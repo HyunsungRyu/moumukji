9fa01b0b794c5f6bf0629251d07ee65c8b140683
HyunsungRyu <seal0130@gmail.com>
2023-11-16 11:46:07 +0900

    20231116

--- moumukji.py
+++ moumukji.py
@@ -8,12 +8,14 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer
 
+# 데이터 로드
 main_dish = pd.read_csv("main_dish.csv")
 side_dish = pd.read_csv("side_dish.csv")
 kimchi = pd.read_csv("removed_kimchi.csv")
 rice = pd.read_csv("removed_rice.csv")
 soup = pd.read_csv("removed_soup.csv")
 
+# 필요한 열 선택
 columns = [
     "RecipeID",
     "Name",
@@ -30,6 +32,7 @@ kimchi_dataset = kimchi[columns]
 rice_dataset = rice[columns]
 soup_dataset = soup[columns]
 
+# 무작위 식사 생성
 random_meal = []
 for i in range(100):
     meal_list = [
@@ -41,6 +44,7 @@ for i in range(100):
     ]
     random_meal.append(meal_list)
 
+# 식사 조합 및 영양소 계산
 random_meal_nutrients = {
     "calories": [],
     "carbohydrate": [],
@@ -70,6 +74,7 @@ for i in range(len(random_meal)):
     for nutrient in random_meal_nutrients:
         random_meal_nutrients[nutrient].append(meal_nutrient_sum[nutrient])
 
+# 일일 최대 영양소 값 정의
 max_daily_Calories = 2700
 max_daily_Carbohydrate = 325
 max_daily_Protein = 200
@@ -85,6 +90,7 @@ max_list = [
     max_daily_Sodium,
 ]
 
+# 식사 당 최대 영양소 값 정의
 max_one_meal_Calories = 1500
 max_one_meal_Carbohydrate = 200
 max_one_meal_Protein = 150
@@ -100,6 +106,7 @@ max_one_meal_list = [
     max_one_meal_Sodium,
 ]
 
+# 영양소가 최대값을 초과하는 식사 제거
 filtered_random_meal = random_meal.copy()
 for i in range(len(filtered_random_meal)):
     for j in range(6):
@@ -111,6 +118,7 @@ for i in range(len(filtered_random_meal)):
 
 filtered_random_meal = [i for i in filtered_random_meal if i != 0]
 
+# 필터링된 식사의 영양소 계산
 filtered_meal_nutrients = {nutrient: [] for nutrient in random_meal_nutrients}
 
 for i in range(len(filtered_random_meal)):
@@ -126,6 +134,7 @@ for i in range(len(filtered_random_meal)):
     for nutrient in filtered_meal_nutrients:
         filtered_meal_nutrients[nutrient].append(meal_nutrient_sum[nutrient])
 
+# 최종 식사 데이터프레임 생성 및 CSV로 저장
 final_meal = pd.DataFrame()
 final_meal["RecipeID"] = filtered_random_meal
 for nutrient in filtered_meal_nutrients:
@@ -133,22 +142,76 @@ for nutrient in filtered_meal_nutrients:
 
 final_meal.to_csv("final_meal.csv")
 
-correlation_matrix = final_meal.iloc[:, 1:].corr()
+# 최종 식사들에 대한 모든 조합 생성
+day_meal = []
 
+for i in range(1, len(final_meal) - 2):
+    for j in range(i + 1, len(final_meal) - 1):
+        for k in range(j + 1, len(final_meal)):
+            meal_combination = [
+                final_meal.iloc[i, :],
+                final_meal.iloc[j, :],
+                final_meal.iloc[k, :],
+            ]
+            day_meal.append(meal_combination)
+
+# k-NN 모델을 사용하여 추천된 식사를 찾고 출력
+closest_meal_combination = None
+closest_distance = float("inf")
+
+# Scaler 정의
 scaler = StandardScaler()
+
+# 데이터 정규화
 final_data = scaler.fit_transform(final_meal.iloc[:, 1:].to_numpy())
 
-neigh = NearestNeighbors(metric="cosine", algorithm="brute")
+# k-NN 모델 정의
+neigh = NearestNeighbors(metric="euclidean", algorithm="brute")
 neigh.fit(final_data)
 
-transformer = FunctionTransformer(neigh.kneighbors, kw_args={"return_distance": False})
-pipeline = Pipeline([("std_scaler", scaler), ("NN", transformer)])
-params = {"n_neighbors": 3, "return_distance": False}
-pipeline.set_params(NN__kw_args=params)
+for meal_combination in day_meal:
+    # 선택한 식사들을 데이터로 준비
+    selected_meals_data = np.vstack([meal.iloc[1:].values for meal in meal_combination])
 
-test_input = final_meal.iloc[0:1, 1:].to_numpy()
-recommended_meal_index = pipeline.transform(test_input)[0][0]
-recommended_meal = final_meal.iloc[recommended_meal_index]
+    # 동일한 scaler를 사용하여 입력 데이터를 정규화
+    selected_meals_data_scaled = scaler.transform(selected_meals_data)
 
-print("Recommended Meal:")
-print(recommended_meal)
+    # 각 선택된 식사에 대한 최근접 이웃 찾기
+    nearest_neighbors_indices = neigh.kneighbors(
+        selected_meals_data_scaled, n_neighbors=1, return_distance=False
+    )
+
+    # 최근접 이웃의 인덱스
+    nearest_neighbor_index = nearest_neighbors_indices[0][0]
+
+    # 최근접 이웃의 영양소 값
+    nearest_neighbor_nutrients = final_meal.iloc[nearest_neighbor_index, 1:]
+
+    # 선택한 식사들의 합산 영양소 계산
+    selected_meals_nutrients_sum = {nutrient: 0 for nutrient in random_meal_nutrients}
+    for meal in meal_combination:
+        for nutrient in selected_meals_nutrients_sum:
+            selected_meals_nutrients_sum[nutrient] += meal[nutrient]
+
+    # 최근접 이웃과 선택한 식사들의 합산 영양소 간의 유클리디안 거리 계산
+    distance = np.linalg.norm(
+        list(selected_meals_nutrients_sum.values()) - nearest_neighbor_nutrients
+    )
+
+    # 현재까지의 최적 조합 업데이트
+    if distance < closest_distance and all(nearest_neighbor_nutrients <= max_list):
+        closest_distance = distance
+        closest_meal_combination = meal_combination
+
+# 결과 출력
+if closest_meal_combination is not None:
+    print("\nSelected Meals:")
+    for i, meal in enumerate(closest_meal_combination):
+        print(f"Meal {i + 1}:")
+        print(meal)
+
+    print("\nRecommended Meal (Closest Neighbor):")
+    print(final_meal.iloc[nearest_neighbor_index, :])
+    print("-------------------------------")
+else:
+    print("No valid combination found within the specified constraints.")
